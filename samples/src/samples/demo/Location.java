1c6a6efc15830211af5d60f113b224fd99e5c69c
Iafiqs <iafiqshome@gmail.com>
2022-06-09 15:31:38 +0800

    Adding Location attributes

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -3,6 +3,15 @@ package samples.demo;
 public class Location 
 {	
 	private String name = "Mike";
+	private String place = "Melaka";
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
 
 	public String getName() {
 		return name;
